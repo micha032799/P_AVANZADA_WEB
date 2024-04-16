e8356facec6813e491c03d0330ef86e27bcd8644
micha032799 <103457034+micha032799@users.noreply.github.com>
2024-04-15 20:19:06 -0600

    PreguntasFrecuentes

---
+++ API_P_Avanzada_W/API_Progra_AvanzadaW/Controllers/PreguntasFrecuentesController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using API_Progra_AvanzadaW.Entidades;
+using Dapper;
+
+namespace API_Progra_AvanzadaW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreguntasFrecuentesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private string _connection;
+
+        public PreguntasFrecuentesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = _configuration.GetConnectionString("DefaultConnection");
+
+        }
+        [HttpPost]
+        [Route("RegistrarPreguntaFrecuente")]
+        public IActionResult RegistrarPreguntaFrecuente(PreguntasFrecuentes entidad)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+
+                    var datos = context.Execute("RegistrarPreguntaFrecuente",
+                        new
+                        {
+                            entidad.IdUsuario,
+                            entidad.Pregunta,
+                            entidad.Respuesta
+                        },
+                        commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [HttpPut]
+        [Route("ActualizarPreguntasFrecuentes")]
+        public IActionResult ActualizarPreguntasFrecuentes(PreguntasFrecuentes entidad)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Execute("ActualizarPreguntasFrecuentes",
+                        new
+                        {
+                            entidad.IdPreguntasFrecuentes,
+                            entidad.Pregunta,
+                            entidad.Respuesta
+
+                        },
+                        commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("ObtenerPreguntaFrecuentePorId")]
+        public IActionResult ObtenerPreguntaFrecuentePorId(long q)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<PreguntasFrecuentes>("ObtenerPreguntaFrecuentePorId",
+                     new { IdPreguntasFrecuentes = q },
+                     commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("EliminarPreguntaFrecuentePorId")]
+        public IActionResult EliminarPreguntaFrecuentePorId(long q)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Execute("EliminarPreguntaFrecuentePorId",
+                       new { IdPreguntasFrecuentes = q },
+                       commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+        [HttpGet]
+        [Route("ObtenerTodasLasPreguntasFrecuentes")]
+        public IActionResult ObtenerTodasLasPreguntasFrecuentes()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<PreguntasFrecuentes>("ObtenerTodasLasPreguntasFrecuentes",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ P_Avanzada_W/Progra_Avanzada_W/Controllers/PreguntasFrecuentesController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progra_Avanzada_W.Entidades;
+using Progra_Avanzada_W.Models;
+using Progra_Avanzada_W.Services;
+
+namespace Progra_Avanzada_W.Controllers
+{
+    [Seguridad]
+    [ResponseCache(NoStore = true, Duration = 0)]
+    public class PreguntasFrecuentesController : Controller
+    {
+        private readonly IPreguntasFrecuentesModel _PreguntasFrecuentesModel;
+        public PreguntasFrecuentesController(IPreguntasFrecuentesModel PreguntasFrecuentesModel)
+        {
+            _PreguntasFrecuentesModel = PreguntasFrecuentesModel;
+        }
+
+        [HttpGet]
+        public IActionResult RegistrarPreguntaFrecuente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistrarPreguntaFrecuente(PreguntasFrecuentes entidad)
+        {
+          
+            string idUsuarioString = HttpContext.Session.GetString("IdUsuario");
+            if (long.TryParse(idUsuarioString, out long idUsuario))
+            {
+                entidad.IdUsuario = idUsuario;
+            }
+            var resp = _PreguntasFrecuentesModel.RegistrarPreguntaFrecuente(entidad);
+
+            if (resp == 1)
+                return RedirectToAction("ObtenerTodasLasPreguntasFrecuentes", "PreguntasFrecuentes");
+            else
+            {
+                ViewBag.MensajePantalla = "No se logró registrar la pregunta frecuente";
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult ObtenObtenerPreguntaFrecuentePorIderConsultaPorId()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ObtenerPreguntaFrecuentePorId(long q)
+        {
+            var datos = _PreguntasFrecuentesModel.ObtenerPreguntaFrecuentePorId(q);
+            return View(datos);
+        }
+
+
+        [HttpGet]
+        public IActionResult ActualizarPreguntasFrecuentes(long q)
+        {
+            var datos = _PreguntasFrecuentesModel.ObtenerTodasLasPreguntasFrecuentes().Where(x => x.IdPreguntasFrecuentes == q).FirstOrDefault();
+            return View(datos);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult ActualizarPreguntasFrecuentes(PreguntasFrecuentes entidad)
+        {
+            var resp = _PreguntasFrecuentesModel.ActualizarPreguntasFrecuentes(entidad);
+
+            if (resp == 1)
+            {
+                return RedirectToAction("ObtenerTodasLasPreguntasFrecuentes", "PreguntasFrecuentes");
+            }
+            else
+            {
+                ViewBag.MensajePantalla = "No se logró actualizar la pregunta frecuente";
+                return View();
+            }
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ObtenerTodasLasPreguntasFrecuentes()
+        {
+
+            var datos = _PreguntasFrecuentesModel.ObtenerTodasLasPreguntasFrecuentes();
+            return View(datos);
+
+        }
+
+        [HttpGet]
+        public IActionResult EliminarPreguntaFrecuentePorId(long q)
+        {
+            _PreguntasFrecuentesModel.EliminarPreguntaFrecuentePorId(q);
+
+            return RedirectToAction("ObtenerTodasLasPreguntasFrecuentes", "PreguntasFrecuentes");
+        }
+    }
+}
+
